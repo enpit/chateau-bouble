ad00703efd93b093495c3bb44f0d3df9f160a513
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-24 17:15:54 +0200

    send message on enter

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const TextInput = ({text, onSubmit, onUpdateMessage}) => (
   <div className="text-view">
-    <input type="text" value={text} onChange={(event) => onUpdateMessage(event.target.value)} />
+    <input type="text"
+      value={text}
+      onChange={(event) => onUpdateMessage(event.target.value)}
+      onKeyUp={(event) => {
+        if (event.keyCode === 13) {
+          onSubmit(text);
+        }
+      }}/>
     <button onClick={() => onSubmit(text)}>></button>
   </div>
 );
