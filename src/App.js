f3c7c852f2968c393a06353207488f8f862960a3
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-24 11:00:47 +0200

    boostrap Chat container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
-import ChatView from './components/ChatView';
+import Chat from './containers/Chat';
 import store from './store';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ChatView messages={[]} />
+        <Chat />
       </Provider>
     );
   }
