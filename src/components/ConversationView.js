3acb396e2e120e4b3b4455c2549645ba0f4b7d80
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-24 22:00:04 +0200

    align spacing

--- src/components/ConversationView.js
+++ src/components/ConversationView.js
@@ -2,23 +2,22 @@ import React from 'react';
 import Message from './Message';
 import styled from 'styled-components';
 
-const ConversationView = styled.ul`
+const ConversationView = styled.ul `
     list-style-type: none;
     padding-left: 0;
     padding-top: 1em;
 `;
 
-const conversationview = function ({messages, user}) {
+const conversationview = function({messages, user}) {
 
-    const bubbles = messages.map((message, index) => {
-        return (
-            <li key={index}><Message author={message.author} text={message.text} time={message.time} isOwnMessage={message.author === user}/></li>
-        );
-    });
-
+  const bubbles = messages.map((message, index) => {
     return (
-        <ConversationView>{bubbles}</ConversationView>
+      <li key={index}><Message author={message.author} text={message.text} time={message.time} isOwnMessage={message.author === user}/></li>
     );
+  });
+  return (
+    <ConversationView>{bubbles}</ConversationView>
+  );
 };
 
 export default conversationview;
