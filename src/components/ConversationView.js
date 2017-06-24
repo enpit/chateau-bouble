568d6d76a28bfdd7aff773389bbe75918806ced5
objectliteral <objectliteral@outlook.com>
2017-06-25 00:45:21 +0200

    Fixed scrolling only working for a couple of messages, now also in Chrome, possibly

--- src/components/ConversationView.js
+++ src/components/ConversationView.js
@@ -14,7 +14,7 @@ const ConversationView = styled.ul`
 class conversationview extends React.Component {
 
   componentDidUpdate () {
-    this.el.parentNode.scrollTop = this.el.parentNode.scrollTopMax;
+    this.el.parentNode.scrollTop = this.el.scrollHeight;
   }
 
   render () {
