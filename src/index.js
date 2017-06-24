398726338c7b6e112e414176b27576f5f415c864
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-24 12:09:24 +0200

    divide demo and chatbbuble-lib files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Demo from './Demo';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('root'));
 registerServiceWorker();
