9c6524d519ac48483d7edb12de0a81fa948b6377
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-25 13:59:37 +0200

    Add a chateauBuble theme

--- src/reducers/themes.js
+++ src/reducers/themes.js
@@ -21,6 +21,15 @@ const INITIAL_STATE = {
       height: '480px',
       width: '420px'
     }
+  },
+  chateauBuble: {
+    backgrounds: ['#fff', '#ededed'],
+    text: ['#000', '#b2b2b2', '#8c9943', '#999'],
+    highlights: ['#70d6ff', '#e9ff70', '#56acc9'],
+    dimensions: {
+      height: '480px',
+      width: '420px'
+    }
   }
 }
 
