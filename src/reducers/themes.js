b1166bc6a10e7b0a063684817ce3c0488949d4fe
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-25 13:58:35 +0200

    Add a green theme

--- src/reducers/themes.js
+++ src/reducers/themes.js
@@ -1,8 +1,22 @@
+/*
+highlights: [OwnBubble, ForeignBubble, Status],
+text: [,,ForeignAuthor,]
+ */
+
 const INITIAL_STATE = {
   current: {
     backgrounds: ['#fff', '#ededed'],
     text: ['#000', '#b2b2b2', '#8c9943', '#999'],
     highlights: ['#70d6ff', '#e9ff70', '#56acc9'],
+    dimensions: {
+      height: '480px',
+      width: '420px'
+    }
+  },
+  green: {
+    backgrounds: ['#fff', '#ededed'],
+    text: ['#000', '#b2b2b2', '#25827e', '#999'],
+    highlights: ['#71f79f', '#3dd6d0', '#3dd6d0'],
     dimensions: {
       height: '480px',
       width: '420px'
