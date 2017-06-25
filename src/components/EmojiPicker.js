ab684387ca7ca48b21400062e0f5835284ce8864
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-25 23:18:50 +0200

    calc correct tooltip position on pageload

--- src/components/EmojiPicker.js
+++ src/components/EmojiPicker.js
@@ -25,21 +25,49 @@ const EmojiLogo = styled(VerticalCenteredDiv)`
   }
 `;
 
-const EmojiPicker = ({ onPickEmoji }) => (
-  <div>
-    <Tooltip
-      placement="topRight"
-      mouseEnterDelay={0}
-      mouseLeaveDelay={0.5}
-      destroyTooltipOnHide={true}
-      trigger={['hover']}
-      overlay={<Picker onClick={onPickEmoji}/>}
-    >
-      <EmojiLogo>
-        <Emoji emoji="smiley" size={20} set="emojione"/>
-      </EmojiLogo>
-    </Tooltip>
-  </div>
-);
+class EmojiPicker extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.pickerX = 0;
+  }
+
+  componentDidMount () {
+    const rect = this.div.getBoundingClientRect();
+    const bodyRect = document.body.getBoundingClientRect();
+    const pickerWidth = 338;
+    let x = rect.right - pickerWidth/2;
+    if (x + pickerWidth > bodyRect.width) { // picker overflows to the right
+      x += bodyRect.width - (x + pickerWidth) - 10;
+    }
+    this.pickerX = x;
+    this.forceUpdate();
+  }
+
+  render () {
+    const PickerContainer = styled.div`
+      position: relative;
+      left: ${this.pickerX}px !important;
+    `;
+    console.log(this.right);
+    const { onPickEmoji } = this.props;
+    return (
+      <div ref={(el) => this.div = el }>
+        <Tooltip
+          placement="topRight"
+          mouseEnterDelay={0}
+          mouseLeaveDelay={0.5}
+          destroyTooltipOnHide={true}
+          trigger={['click']}
+          overlay={<PickerContainer><Picker onClick={onPickEmoji}/></PickerContainer>}
+        >
+          <EmojiLogo>
+            <Emoji emoji="smiley" size={20} set="emojione"/>
+          </EmojiLogo>
+        </Tooltip>
+      </div>
+    );
+  }
+}
 
 export default EmojiPicker;
