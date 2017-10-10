ffc47348877821acbdad9fe9c5f489a85b0a2a75
Peter Steinberg <objectliteral@outlook.com>
2017-10-10 15:49:41 +0200

    Fixed default theme name not being properly set

--- src/ChateauBouble.js
+++ src/ChateauBouble.js
@@ -4,7 +4,7 @@ import ChatView from './components/ChatView';
 import Themes from './themes';
 
 const ChateauBouble = (props) => {
-    const theme = Themes[props.themeKey || 'chateauBouble'];
+    const theme = Themes[props.themeKey || 'default'];
 
     return (
         <ChatView {...props} theme={theme} />
